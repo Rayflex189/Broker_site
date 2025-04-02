f027d05153adb6b6cf33beb856e28af77c2aa29b
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-02 05:35:52 +0100

    Update apps.py
    Save

--- broker_site/broker_app/apps.py
+++ broker_site/broker_app/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class BrokerAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'broker_app'
+
+    def ready(self):
+        import broker_app.signals  # Ensures signals are loaded when the app starts
